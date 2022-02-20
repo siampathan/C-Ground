d019d97a86924389273e7ea0165580453ad16c11
siampathan005@gmail.com <siampathan005@gmail.com>
2022-02-20 10:54:42 +0600

    ATM-application convert to OOP concept

--- ATM_application/atm_app.cpp
+++ ATM_application/atm_app.cpp
@@ -1,23 +1,33 @@
 #include<iostream>
 using namespace std;
 
-void menu() {
+class ATM {
+	double city,diposite,withdraw;
+	public :
+		int choice;
+		ATM () {
+			city = 0 , diposite = 0 , withdraw = 0;
+		}
+		
+		void menu();
+		void calculation ();
+		
+};
+
+void ATM :: menu() {
 	cout<<"\n\t***********MENU************"<<endl;
 	cout<<"\t	1.for diposite "<<endl;
 	cout<<"\t	2.for withdraw "<<endl;
-	cout<<"\t	3.for displaty "<<endl;
+	cout<<"\t	3.for display "<<endl;
 	cout<<"\t	4.for exit "<<endl;
 	cout<<"\n\t****************************"<<endl;
 }
 
-int main () {
-	double city,diposite,withdraw;
-	int choice;
+void ATM :: calculation () {
 	
 	cout<<"\tEnter your bank amount : ";
 	cin>>city;
 	
-	menu();
 	cout<<"\n\tYour accout is ready now, your account balance is : "<<city<<endl;
 	do {
 		cout<<"\n\tEnter your choice : ";
@@ -48,6 +58,14 @@ int main () {
 			cout<<"\n\tInvalid choice "<<endl;
 	} while(choice != 4);
 	
+}
+
+int main () {
+	
+	ATM item_obj;
+	
+	item_obj.menu();
+	item_obj.calculation();
 	
 	return 100;
 }
